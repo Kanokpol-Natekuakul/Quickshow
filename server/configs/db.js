6389676051ee185f97c78f1ce9b5adcc00235be0
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2025-09-09 19:20:45 +0700

    test

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
 
-const connectDB=async()=>{
+const connectDB = async () => {
     try {
-        mongoose.connection.on('connected',()=>console.log('Database connected'))
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+        mongoose.connection.on('connected', () => console.log('Database connected'));
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
     } catch (error) {
-        console.log(error.message)
+        console.log("MongoDB connection error:", error.message);
     }
-}
+};
 
-export default connectDB
+export default connectDB;
